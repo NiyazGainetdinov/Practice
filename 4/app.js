909f8743e51e891a8b9eecf3c437f3d896baff66
Gainetdinov Niyaz Almazovich <noyaz0022@gmail.com>
2021-04-21 12:46:55 +0300

    Работа вкладок

---
+++ 4/app.js
@@ -0,0 +1,25 @@
+var main = function () {
+"use strict";
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1)").on("click", function () {
+/span></a>/span></a> 
+$(".tabs span").removeClass("active");
+/span></a>/span></a> 
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1) span").addClass("active");
+/span></a>/span></a> 
+$("main .content").empty();
+/span></a>/span></a> 
+return false;
+});
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2)").on("click", function () {
+$(".tabs span").removeClass("active");
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2) span").addClass("active");
+$("main .content").empty();
+return false;
+});
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3)").on("click", function () {
+$(".tabs span").removeClass("active");
+$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3) span").addClass("active");
+$("main .content").empty();
+return false;
+});
+};
