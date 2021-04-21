b880e1cd13dd5ffa5a45991d5e8ec093d6febc59
Gainetdinov Niyaz Almazovich <noyaz0022@gmail.com>
2021-04-21 12:56:02 +0300

    Используем цикл

--- 4/app.js
+++ 4/app.js
@@ -1,20 +1,12 @@
 var main = function () {
 "use strict";
-var makeTabActive = function (tabNumber) {
+var tabNumber;
+for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
 var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+$(tabSelector).on("click", function () {
 $(".tabs span").removeClass("active");
-$(tabSelector).addClass("active");
-};
-$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(1)").on("click", function () {
-makeTabActive(1);
+$(this).addClass("active");
 return false;
 });
-$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(2)").on("click", function () {
-makeTabActive(2);
-return false;
-});
-$(".tabs a:nth-код для этого элемента может выглядеть вот так:child(3)").on("click", function () {
-makeTabActive(3);
-return false;
-});
+}
 };
