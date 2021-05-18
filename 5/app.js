ba5a91a9ba3f1b92d24d934ec7f0506fe34e7f59
Gainetdinov Niyaz Almazovich <noyaz0022@gmail.com>
2021-05-18 14:49:18 +0300

    правка файла .js

--- 5/app.js
+++ 5/app.js
@@ -89,25 +89,33 @@ var main = function (toDoObjects) {
 
 			}  
 			else if ($element.parent().is(":nth-child(4)")) { 
-				$(".content").append(
-					'<input type="text" class="inp">'+
-					'<button class="btn">Добавить</button>'
-				);
-				var newToDo;
-				$('.btn').on('click',function(){
-					newToDo= $('.inp').val();
-					if (newToDo!='') {
-						toDos.push( newToDo);
-						alert('Новое задание "'+newToDo+'" успешно добавлено!');
-						$('.inp').val("");
-					}
-				})
-		
+				var $input = $("<input>").addClass("description"), 
+					$inputLabel = $("<p>").text("Новая задача: "),
+					$tagInput = $("<input>").addClass("tags"),
+					$tagLabel = $("<p>").text("Тэги: "),
+					$button = $("<button>").text("+");
+				$button.on("click", function () {
+					var description = $input.val(),
+					// разделение в соответствии с запятыми
+					tags = $tagInput.val().split(","); 
+					toDoObjects.push({"description":description, "tags":tags}); 
+					// обновление toDos
+					toDos = toDoObjects.map(function (toDo) {
+						return toDo.description;
+					});
+					$input.val("");
+					$tagInput.val("");
+				});
+				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button); 
+			}
+			else if ($element.parent().is(":nth-child(5)")) { 
+				var js = document.createElement('script');
+				js.src = "zadanie_app.js";
+				document.body.appendChild(js);
 			}
 			return false;
 		})
 	})
-
 
 	$(".tabs a:first-child span").trigger("click");
 
