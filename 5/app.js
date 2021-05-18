63020167b1f70e730a586bcf6e21e682c678795c
Gainetdinov Niyaz Almazovich <noyaz0022@gmail.com>
2021-05-18 14:25:47 +0300

    изменяем работу вкладок

--- 5/app.js
+++ 5/app.js
@@ -37,8 +37,26 @@ $(".tabs a span").toArray().forEach(function (element) {
 			toDos.forEach(function (todo) { 
 				$(".content").append($("<li>").text(todo));
 			});
-		} 
+		}
 		else if ($element.parent().is(":nth-child(3)")) { 
+				// ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ 
+				console.log("Щелчок на вкладке Теги");
+				// organizeByTag(toDoObjects);
+				var organizedByTag = organizeByTags(toDoObjects);
+				
+				 organizedByTag.forEach(function (tag) { 
+					var $tagName = $("<h3>").text(tag.name), 
+					$content = $("<ul>"); 
+					tag.toDos.forEach(function (description) { 
+						var $li = $("<li>").text(description); 
+						$content.append($li);
+					});
+					$("main .content").append($tagName); 
+					$("main .content").append($content);
+				});
+
+		}  
+		else if ($element.parent().is(":nth-child(4)")) { 
 			$(".content").append(
 				'<input type="text" class="inp">'+
 				'<button class="btn">Добавить</button>'
