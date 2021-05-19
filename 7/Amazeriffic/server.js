a049e861a5aff246de159990aa643bb6d2cdc903
Gainetdinov Niyaz Almazovich <noyaz0022@gmail.com>
2021-05-19 14:23:06 +0300

    дополнительные правки

--- 7/Amazeriffic/server.js
+++ 7/Amazeriffic/server.js
@@ -1,7 +1,12 @@
 var express = require("express"),
 	http = require("http"),
 	mongoose = require("mongoose"),
-	app = express(); 
+	app = express(); /*,
+	toDos = [
+		// настраиваем список задач копированием
+		// содержимого из файла todos.OLD.json
+	];*/
+	// импортируем библиотеку mongoose
 app.use(express.static(__dirname + "/client"));
 // Это модель Mongoose для задач
 var ToDoSchema = mongoose.Schema({
@@ -10,14 +15,27 @@ var ToDoSchema = mongoose.Schema({
 });
 var ToDo = mongoose.model("ToDo", ToDoSchema);
 http.createServer(app).listen(3000);
-app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb://localhost/amazeriffic');
+// Этот маршрут замещает наш файл
+// todos.json в примере из части 5
 app.get("/todos.json", function (req, res) {
 	ToDo.find({}, function (err, toDos) {
 		// не забудьте о проверке на ошибки
 		res.json(toDos);
 	});
 });
+// командуем Express принять поступающие
+// объекты JSON
+app.use(express.urlencoded({ extended: true }));
+// подключаемся к хранилищу данных Amazeriffic в Mongo
+mongoose.connect('mongodb://localhost/amazeriffic', {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true 
+	}).then(res => {
+		console.log("DB Connected!")
+	}).catch(err => {
+		console.log(Error, err.message);
+	});
 app.post("/todos", function (req, res) {
 	console.log(req.body);
 	var newToDo = new ToDo({"description":req.body.description,
