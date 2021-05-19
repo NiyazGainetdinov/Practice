851ef6172c4c2cdeaa242631ef0b9dd28bd7acc4
Gainetdinov Niyaz Almazovich <noyaz0022@gmail.com>
2021-05-19 13:29:35 +0300

    добавили схему в server.js

--- 7/Amazeriffic/server.js
+++ 7/Amazeriffic/server.js
@@ -3,5 +3,12 @@ var express = require("express"),
 	mongoose = require("mongoose"),
 	app = express(); 
 app.use(express.static(__dirname + "/client"));
+// Это модель Mongoose для задач
+var ToDoSchema = mongoose.Schema({
+	description: String,
+	tags: [ String ]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+http.createServer(app).listen(3000);
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/amazeriffic');
